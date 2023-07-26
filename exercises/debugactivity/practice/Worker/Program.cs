78dfd604bfd598113eb1f89e61cfa4ceaa2a0f44
sains1 <nathan.sains@gmail.com>
2023-07-26 18:04:00 +0100

    Adds consistency to temporal client registration

--- exercises/debugactivity/practice/Worker/Program.cs
+++ exercises/debugactivity/practice/Worker/Program.cs
@@ -6,8 +6,6 @@ using Temporalio.Extensions.Hosting;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        // NOTE - instead of using services.AddTemporalClient as in the other exercises, I've had to register the client
-        //          manually in order to customise the LoggerFactory
         services.TryAddSingleton<ITemporalClient>(provider =>
             TemporalClient.CreateLazy(new TemporalClientConnectOptions
             {
--- exercises/usingstructs/solution/Worker/Program.cs
+++ exercises/usingstructs/solution/Worker/Program.cs
@@ -1,13 +1,17 @@
 using Application;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Temporalio.Client;
 using Temporalio.Extensions.Hosting;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        services.AddTemporalClient(x =>
-        {
-            x.TargetHost = "localhost:7233";
-        });
+        services.TryAddSingleton<ITemporalClient>(provider =>
+            TemporalClient.CreateLazy(new TemporalClientConnectOptions
+            {
+                TargetHost = "localhost:7233",
+                LoggerFactory = provider.GetRequiredService<ILoggerFactory>()
+            }));
 
         services.AddHostedTemporalWorker(WorkflowConstants.TaskQueueName)
             .AddStaticActivities(typeof(Activities))
